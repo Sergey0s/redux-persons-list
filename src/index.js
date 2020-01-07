219e8bba2c906c2754c66c7510b499a675f62dfb
SergeyOs <mediaworld@mail.ru>
2020-01-07 23:26:47 +0300

    Remove typo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import personReducer from '../src/store/reducer';
-----
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
