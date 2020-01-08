5cf96dd567cf2ad75db6303b8f9e07d342a41914
SergeyOs <mediaworld@mail.ru>
2020-01-08 14:21:49 +0300

    fix task and how-to paragraphs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 import Persons from './containers/Persons';
 
+const style = {
+    display: "inline-block",
+    textAlign: "center",
+    margin: "16px",
+    width: "100%"
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ol>
-          <li>Task: Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
+          <li style={style}>Task: Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
+            <li style={style}>Tap on "Add Person" to create a new person then tap on it to delete</li>
         </ol>
         <Persons />
       </div>
